3aa7afed940370562985cff3d41bceb5724741f7
DDanii <ddani1995@gmail.com>
2018-04-24 08:05:38 +0200

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
 const login = require("facebook-chat-api");
 
-login({email: "FB_EMAIL", password: "FB_PASSWORD"}, (err, api) => {
+login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
+    // Here you can use the api
+    api.listen((err, message) => {
+        if(err) return console.error(err);
 
-    fs.writeFileSync('/stuff/lock/appstate.json', JSON.stringify(api.getAppState()));
+        // Ignore empty messages (photos etc.)
+        if (typeof message.body === "string") {
+            api.sendMessage(message.body, message.threadID);
+        }
+    });
 });
