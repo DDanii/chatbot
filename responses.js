4e60160ea72f5b230b27d989e771b47dbf47d7a1
DDani <ddani1995@gmail.com>
2018-04-24 15:51:37 +0200

    update

--- responses.js
+++ responses.js
@@ -1,7 +1,8 @@
+const shell = require("shelljs");
 const fs = require("fs");
 const login = require("facebook-chat-api");
 
-login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) => {
+login({appState: JSON.parse(fs.readFileSync('/stuff/lock/appstate.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
     // Here you can use the api
     api.listen((err, message) => {
@@ -11,10 +12,14 @@ login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, ap
         if (typeof message.body === "string") {
             if(message.body == ":A")
                         api.sendMessage("( ͡° ͜ʖ ͡°)", message.threadID);
-		if(message.body == "skip") }
-                        shell.exec('/bin/skip'); 
-		if(message.body.startsWith("next ")) });
-                        shell.exec('/bin/next '+message.body.substring(5));});
+		if(message.body == "skip")
+                        shell.exec('/bin/skip');
+		if(message.body.startsWith("next "))
+                        shell.exec('/bin/next '+message.body.substring(5));
                 if(message.body.toLowerCase() == "current"){
                         cur=shell.exec('current');
-			api.sendMessage(
+			api.sendMessage(cur.toString() , message.threadID);
+		}
+	}
+});
+});
