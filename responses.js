5541d0e61230a4b58a4218124a5abc871c26b9ef
DDani <ddani1995@gmail.com>
2018-05-27 14:31:31 +0200

    update

--- responses.js
+++ responses.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const login = require("facebook-chat-api");
 
 login({appState: JSON.parse(fs.readFileSync('/stuff/lock/appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
+    if(err){console.log("asd"); return console.error(err);}
     // Here you can use the api
     api.listen((err, message) => {
-        if(err) return console.error(err);
+        if(err){console.log("asd2"); return console.error(err); }
 
         // Ignore empty messages (photos etc.)
         if (typeof message.body === "string") {
@@ -31,6 +31,10 @@ login({appState: JSON.parse(fs.readFileSync('/stuff/lock/appstate.json', 'utf8')
 			api.sendMessage({attachment: fs.createReadStream("/stuff/asd.mp4") }, message.threadID);
 		if(message.body == "rip0.mp4")
                         api.sendMessage({attachment: fs.createReadStream("/stuff/rip0.gif") }, message.threadID);
+		if(message.body.split(".")[1] == "gif" )
+			api.sendMessage({attachment: fs.createReadStream("/stuff/" + message.body.split(".")[0] + "." + message.body.split(".")[1])}, message.threadID);
+		if(message.body.toLowerCase() == "pubip")
+			api.sendMessage((shell.exec("pubip")).toString(), message.threadID);
 	}
 	});
 });
