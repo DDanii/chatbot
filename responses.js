e2619374b4d3c249bdc254f28d292b9336ac19ee
DDani <ddani1995@gmail.com>
2018-07-03 18:54:34 +0200

    update

--- responses.js
+++ responses.js
@@ -20,13 +20,15 @@ login({appState: JSON.parse(fs.readFileSync('/stuff/lock/appstate.json', 'utf8')
                         cur=shell.exec('current');
 			api.sendMessage(cur.toString() , message.threadID);
 		}
-		if(message.body.startsWith("math") || message.body.startsWith("Math")){
+		if(message.body.toLowerCase().startsWith("math") && !message.body.contains("|")){
 			cur=shell.exec("math "+ message.body.split(" ")[1]);
 			api.sendMessage(cur.toString() , message.threadID);
 		}
 		if(message.body.toLowerCase() == "dunno"){
 			api.sendMessage("¯\\_(ツ)_/¯", message.threadID);
 		}
+		if(message.body.toLowerCase() == "threadid")
+			api.sendMessage(message.threadID, message.threadID);
 		if(message.body == "asd.mp4")
 			api.sendMessage({attachment: fs.createReadStream("/stuff/asd.mp4") }, message.threadID);
 		if(message.body == "rip0.mp4")
@@ -38,6 +40,26 @@ login({appState: JSON.parse(fs.readFileSync('/stuff/lock/appstate.json', 'utf8')
 		if(message.body.toLowerCase() == "send nudes"){
 			api.sendTypingIndicator(message.threadID);
 			api.sendMessage({attachment: fs.createReadStream((shell.exec("getp")).toString()) }, message.threadID);
+		}
+		if(message.body.toLowerCase() == "send memes"){
+                        api.sendTypingIndicator(message.threadID);
+                        api.sendMessage({attachment: fs.createReadStream((shell.exec("rmeme")).toString()) }, message.threadID );
+		}
+		if(message.body.toLowerCase() == "send more"){
+			api.sendTypingIndicator(message.threadID);
+                        api.sendMessage({attachment: fs.createReadStream((shell.exec("getp")).toString()) }, message.threadID);
+			api.sendTypingIndicator(message.threadID);
+                        api.sendMessage({attachment: fs.createReadStream((shell.exec("getp")).toString()) }, message.threadID);
+			api.sendTypingIndicator(message.threadID);
+                        api.sendMessage({attachment: fs.createReadStream((shell.exec("getp")).toString()) }, message.threadID);
+		}
+		var rep=message.body.split(";");
+		console.log(rep.length);
+		if(message.body.split(";").length==3 && message.body.split(";")[0].toLowerCase()=="repeate"){
+			let tmp="";
+			for(let i=0;message.body.split(";")[1]>i;i++)
+				tmp+=message.body.split(";")[2];
+			api.sendMessage(tmp, message.threadID);
 		}
 	}
 	});
